733bc104e8e37241b2c91cb7fd61f452af935e49
ammarmsd123 <i238210@isb.nu.edu.pk>
2025-06-03 09:30:52 +0500

    saperated DB

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ import os
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL") or \
+    "postgresql://username:password@hostname:5432/dbname"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
